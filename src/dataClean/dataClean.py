d5fef0ae9268e9d90a7032b1798a749057e60fef
juaca2009 <juaca2009@gmail.com>
2021-09-18 14:57:57 -0500

    serializacion atributos

--- src/dataClean/dataClean.py
+++ src/dataClean/dataClean.py
@@ -1,5 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline
 
 #definicion funciones
 def eliminarColumnas(_data, _Lcolumnas):
@@ -7,6 +9,12 @@ def eliminarColumnas(_data, _Lcolumnas):
 
 def eliminarColumnasNulos(_data, _Lcolumnas):
     return _data.dropna(how='any', subset=_Lcolumnas)
+
+def serializarColumnas(_data, _Lcolumnas):
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(df) for column in _Lcolumnas]
+    pipeline = Pipeline(stages=indexers)
+    dfTemp = pipeline.fit(df).transform(df)
+    return dfTemp
 
 
 if __name__ == "__main__":
@@ -21,6 +29,10 @@ if __name__ == "__main__":
     dataColumnas = eliminarColumnas(df, columnasEliminar)     
 
     #elimanar filas con valores nulos 
-    columnasNulos = ['Case Number', 'District', 'Community Area', 'X Coordinate', 'Y Coordinate', 'Location Description']
+    columnasNulos = ['District', 'Community Area', 'X Coordinate', 'Y Coordinate', 'Location Description']
     dataSNula = eliminarColumnasNulos(dataColumnas, columnasNulos)
-    
+
+    #serializar columnas
+    columnasSerializar = ['IUCR', 'Location Description', 'FBI Code']
+    dataTemp = serializarColumnas(dataSNula, columnasSerializar) 
+    dataSerializada = eliminarColumnas(dataTemp, columnasSerializar) #eliminar columnas no serializadas
