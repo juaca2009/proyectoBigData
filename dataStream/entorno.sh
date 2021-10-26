db6639e8c0a98b396c1f6b31f5ffc6755e164b2e
juaca2009 <juaca2009@gmail.com>
2021-10-25 19:29:44 -0500

    script para iniciar entorno python

---
+++ dataStream/entorno.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+iniciar_entorno(){
+	
+	cd /home/juan/Documentos/"entornos python"/servicioKafka/bin
+	source activate
+	cd ../../../proyectos/proyectoBigData/dataStream
+}
+
+iniciar_entorno
+
