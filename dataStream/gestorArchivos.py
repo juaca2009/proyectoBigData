dfc8df5b486ff02ac31938953c4512cb42069b99
juaca2009 <juaca2009@gmail.com>
2021-10-26 23:14:52 -0500

    configuracion datos por fecha

--- dataStream/gestorArchivos.py
+++ dataStream/gestorArchivos.py
@@ -5,13 +5,15 @@ class gestorArchivos:
     def __init__(self, _ruta):
         self.__ruta = _ruta
 
-    def cargarArchivos(self):
+    def cargarArchivos(self, _mes):
         archivos = glob.glob(self.__ruta + '*.csv')
         listaDataframes = list()
         for i in archivos:
             dataTemp = pd.read_csv(i, sep = ',',  header='infer')
             listaDataframes.append(dataTemp)
         dataTotal = pd.concat(listaDataframes, ignore_index=True)
+        dataMes = dataTotal.loc[:, 'mesDel'] == _mes
+        dataTotal = dataTotal.loc[dataMes]
         return dataTotal
 
     def getRuta(self):
@@ -24,5 +26,5 @@ class gestorArchivos:
 
 if __name__ == '__main__':
     a = gestorArchivos('../data/dataClean/')
-    print(a.cargarArchivos())
+    print(a.cargarArchivos(1))
 
