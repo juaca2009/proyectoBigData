478029568ff3f29999900d4f7a45dec324d59c70
juaca2009 <juaca2009@gmail.com>
2021-11-06 13:25:55 -0500

    prueba e instalacion de graphframe

---
+++ src/grafos/prueba.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from graphframes import *
+
+spark=SparkSession.builder.appName('pruebasGrafos').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+v = spark.createDataFrame([
+  ("a", "Alice", 34),
+  ("b", "Bob", 36),
+  ("c", "Charlie", 30),
+], ["id", "name", "age"])
+# Create an Edge DataFrame with "src" and "dst" columns
+e = spark.createDataFrame([
+  ("a", "b", "friend"),
+  ("b", "c", "follow"),
+  ("c", "b", "follow"),
+], ["src", "dst", "relationship"])
+
+g = GraphFrame(v, e)
+
+g.inDegrees.show()
+
+g.edges.filter("relationship = 'follow'").count()
+
+results = g.pageRank(resetProbability=0.01, maxIter=20)
+results.vertices.select("id", "pagerank").show()
+
+#comando para ejecutar desde servidor (ejecutar desde la carpeta /usr)
+#spark-2.4.1/bin/spark-submit --master spark://master:7077 --packages graphframes:graphframes:0.8.1-spark2.4-s_2.11 --repositories https://repos.spark-packages.org /usr/src/grafos/prueba.py
