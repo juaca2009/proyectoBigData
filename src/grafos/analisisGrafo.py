bd39ea2d408f7ee225e9dddb778f6c7e06381c44
juaca2009 <juaca2009@gmail.com>
2021-11-20 14:14:41 -0500

    organizacion codigo

--- src/grafos/analisisGrafo.py
+++ src/grafos/analisisGrafo.py
@@ -54,72 +54,72 @@ def obtencionColegios(g, crimenesDf):
 
 
 
+if __name__ == '__main__':
+    spark=SparkSession.builder.appName('pruebasGrafos').getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
 
-spark=SparkSession.builder.appName('pruebasGrafos').getOrCreate()
-spark.sparkContext.setLogLevel('WARN')
+    #carga de datos
+    estacionesDf = spark.read.csv("/tmp/data/police-stations.csv", sep=',', header= True, inferSchema=True)
+    colegiosDf = spark.read.csv("/tmp/data/chicago-public-schools-elementary-school.csv", sep=',', header=True, inferSchema=True)
+    crimenesDf = spark.read.csv("/tmp/data/dataClean/*.csv", sep=',', header= True, inferSchema=True)
+    colindasDf = spark.read.csv("/tmp/data/dataset_colindantes.csv", sep=';', header=True, inferSchema=True)
 
-#carga de datos
-estacionesDf = spark.read.csv("/tmp/data/police-stations.csv", sep=',', header= True, inferSchema=True)
-colegiosDf = spark.read.csv("/tmp/data/chicago-public-schools-elementary-school.csv", sep=',', header=True, inferSchema=True)
-crimenesDf = spark.read.csv("/tmp/data/dataClean/*.csv", sep=',', header= True, inferSchema=True)
-colindasDf = spark.read.csv("/tmp/data/dataset_colindantes.csv", sep=';', header=True, inferSchema=True)
+    #Creacion dataFrames nodos y conexiones 
+    schemaNodos = StructType() \
+                .add("id", IntegerType()) \
+                .add("tipoNodo", StringType())
 
-#Creacion dataFrames nodos y conexiones 
-schemaNodos = StructType() \
-              .add("id", IntegerType()) \
-              .add("tipoNodo", StringType())
+    schemaConexiones = StructType()\
+                    .add("src", IntegerType()) \
+                    .add("dst", IntegerType()) \
+                    .add("relacion", StringType())
 
-schemaConexiones = StructType()\
-                   .add("src", IntegerType()) \
-                   .add("dst", IntegerType()) \
-                   .add("relacion", StringType())
+    nodos = spark.createDataFrame(spark.sparkContext.emptyRDD(), schemaNodos)
+    conexiones = spark.createDataFrame(spark.sparkContext.emptyRDD(), schemaConexiones)
 
-nodos = spark.createDataFrame(spark.sparkContext.emptyRDD(), schemaNodos)
-conexiones = spark.createDataFrame(spark.sparkContext.emptyRDD(), schemaConexiones)
+    #agregar nodos tipo area comunitaria
+    aComunitarias = crimenesDf.select('Community Area').distinct()
+    aComunitarias = aComunitarias.withColumn("Community Area", aComunitarias["Community Area"].cast(IntegerType()))
+    aComunitarias = aComunitarias.withColumn("tipoNodo", lit("AreaComunitaria"))
+    nodos = nodos.union(aComunitarias)
 
-#agregar nodos tipo area comunitaria
-aComunitarias = crimenesDf.select('Community Area').distinct()
-aComunitarias = aComunitarias.withColumn("Community Area", aComunitarias["Community Area"].cast(IntegerType()))
-aComunitarias = aComunitarias.withColumn("tipoNodo", lit("AreaComunitaria"))
-nodos = nodos.union(aComunitarias)
+    #agregar nodos estaciones de policia 
+    estacionesDf = estacionesDf.dropDuplicates(["ZIP"])
+    estacionesDf = estacionesDf.withColumn("id", estacionesDf["ZIP"]) #generar columna id para cada estacion 
+    estaciones = estacionesDf.select("id").distinct()
+    estaciones = estaciones.withColumn("tipoNodo", lit("EstacionPolicia"))
+    nodos = nodos.union(estaciones)
 
-#agregar nodos estaciones de policia 
-estacionesDf = estacionesDf.dropDuplicates(["ZIP"])
-estacionesDf = estacionesDf.withColumn("id", estacionesDf["ZIP"]) #generar columna id para cada estacion 
-estaciones = estacionesDf.select("id").distinct()
-estaciones = estaciones.withColumn("tipoNodo", lit("EstacionPolicia"))
-nodos = nodos.union(estaciones)
+    #agregar nodos tipo colegios
+    colegios = colegiosDf.select("School ID").distinct()
+    colegios = colegios.withColumn("tipoNodo", lit("Colegio"))
+    nodos = nodos.union(colegios)
 
-#agregar nodos tipo colegios
-colegios = colegiosDf.select("School ID").distinct()
-colegios = colegios.withColumn("tipoNodo", lit("Colegio"))
-nodos = nodos.union(colegios)
+    #agregar conexiones areaC-colegio
+    ac = colegiosDf.select("Community Areas", "School ID")
+    ac = ac.withColumn("relacion", lit("PoseeColegio"))
+    conexiones = conexiones.union(ac)
 
-#agregar conexiones areaC-colegio
-ac = colegiosDf.select("Community Areas", "School ID")
-ac = ac.withColumn("relacion", lit("PoseeColegio"))
-conexiones = conexiones.union(ac)
+    #agregar conexiones areas colindantes
+    aa = colindasDf.withColumn("relacion", lit("Colinda"))
+    conexiones = conexiones.union(aa)
 
-#agregar conexiones areas colindantes
-aa = colindasDf.withColumn("relacion", lit("Colinda"))
-conexiones = conexiones.union(aa)
+    #agregar conexiones areaC-estaciones
+    ae = estacionesDf.select("Community Areas", "id")
+    ae = ae.withColumn("relacion", lit("PoseeEstacion"))
+    conexiones = conexiones.union(ae)
+    conexiones = conexiones.dropna(how='any')
 
-#agregar conexiones areaC-estaciones
-ae = estacionesDf.select("Community Areas", "id")
-ae = ae.withColumn("relacion", lit("PoseeEstacion"))
-conexiones = conexiones.union(ae)
-conexiones = conexiones.dropna(how='any')
-
-g = GraphFrame(nodos, conexiones)
-#imprimirGrafo(conexiones, nodos)
+    g = GraphFrame(nodos, conexiones)
+    #imprimirGrafo(conexiones, nodos)
 
 
-#Obtencion primer feature, estaciones por areas comunitarias
-crimenesDf = obtencionEstaciones(g, aComunitarias, crimenesDf)
+    #Obtencion primer feature, estaciones por areas comunitarias
+    crimenesDf = obtencionEstaciones(g, aComunitarias, crimenesDf)
 
-#Obtencion segundo feature, colegios por areas comunitarias
-crimenesDf = obtencionColegios(g, crimenesDf)
+    #Obtencion segundo feature, colegios por areas comunitarias
+    crimenesDf = obtencionColegios(g, crimenesDf)
 
-#importar datos a csv
-#crimenesDf.write.csv("/tmp/data/dataCleanNew/", header=True)
+    #importar datos a csv
+    #crimenesDf.write.csv("/tmp/data/dataCleanNew/", header=True)
 
